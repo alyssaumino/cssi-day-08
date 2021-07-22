84da48f55c822aabae7ef67da5393b42c45f70af
Alyssa Umino <aumino.contact@gmail.com>
2021-07-22 18:46:32 +0000

    Limits message length

--- public/js/writeMessage.js
+++ public/js/writeMessage.js
@@ -4,10 +4,19 @@ const onSubmit = () => {
     const messageInput = document.querySelector("#message").value;
     //console.log(passcodeInput, messageInput);
 
-    const payload = {
-        passcode: passcodeInput,
-        message: messageInput
+    if(messageInput.length > 20) {
+        alert("Message too long, keep it under 20 characters");
     }
+    else {
+        const payload = {
+            passcode: hex(passcodeInput),
+            message: messageInput
+        }
 
-    firebase.database().ref().push(payload);
+        firebase.database().ref().push(payload);
+    }
+    const passcodeField = document.querySelector("#passcode");
+    passcodeField.value = ""
+    const messageField = document.querySelector("#message");
+    messageField.value = ""
 }
