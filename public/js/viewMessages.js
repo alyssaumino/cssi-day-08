bacd7c296036480e112d99b9d24c5d706148f651
Alyssa Umino <aumino.contact@gmail.com>
2021-07-22 16:46:06 +0000

    Read data from database

--- public/js/viewMessages.js
+++ public/js/viewMessages.js
@@ -1,4 +1,12 @@
 const getMessages = () => {
     const passcode = document.querySelector("#passcode")
-    console.log("button pressed", passcode.value)
+    //console.log("button pressed", passcode.value)
+
+    const messagesRef = firebase.database().ref()
+    messagesRef.on("value", (snapshot) => {
+        const data = snapshot.val()
+        console.log(data)
+
+    })
+
 }
